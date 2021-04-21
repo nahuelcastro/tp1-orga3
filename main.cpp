c9980151491e495a908b3935bdb7b05d45e6e843
Nahuel <>
2021-04-20 22:54:34 -0300

    dynamic programming a medias(solo recursivo hasta ahora), no quiere andar

--- main.cpp
+++ main.cpp
@@ -2,11 +2,9 @@
 #include <vector>
 using namespace std;
 
-typedef unsigned int uint;
-
 struct product{
-    unsigned int weight;
-    unsigned int resistance;
+    int weight;
+    int resistance;
 };
 
 
@@ -15,21 +13,21 @@ bool optimalidad = false;
 bool factibilidad = true;
 
 // jambotubo
-unsigned int quantity_products;
-unsigned int resistance; // resistencia total del tubo
+int quantity_products;
+int resistance; // resistencia total del tubo
 
 vector<bool> mask_products;
 vector<product> products;
-unsigned int result;
+int result;
 
-unsigned int partial_products = 0;
+int partial_products = 0;
 
 // aux: despues borrar, es solo para pruebas
 int aux_count = 0;
 
 // O(n)
-unsigned int quantity_products_jambotubo(){
-    unsigned int acum = 0;
+int quantity_products_jambotubo(){
+    int acum = 0;
     for (int i = 0; i < quantity_products; ++i) {
         if (mask_products[i]){
             acum ++;
@@ -41,9 +39,9 @@ unsigned int quantity_products_jambotubo(){
 
 // O(n)
 bool jambotube_broke(int n){
-    unsigned int local_resistance = 0;
-    unsigned int local_weight = 0;
-    unsigned int total_weight = 0;
+    int local_resistance = 0;
+    int local_weight = 0;
+    int total_weight = 0;
     vector<product> partial_products;
 
     // estas 2 variables despues borrar, son para poder debugear estas variables locales nomas
@@ -88,13 +86,13 @@ void brute_force(int i = 0){
         // estas 3 variables despues borrar, son para poder debugear estas variables locales nomas
         vector<bool> mask_products_copy = mask_products;
         vector<product> products_copy = products;
-        unsigned int result_copy = result;
+        int result_copy = result;
 
         cout << jambotube_broke(i) << endl;  // borrar despues
 
         // O(2.n)
         if (not jambotube_broke(i)){                                         //O(n)
-            unsigned int local_result = 0;
+            int local_result = 0;
             local_result = quantity_products_jambotubo();                   //O(n)
             result = (local_result > result)? local_result : result;
         }
@@ -119,8 +117,8 @@ void backtracking(int i = 0){
     // estas 3 variables despues borrar, son para poder debugear estas variables locales nomas
     vector<bool> mask_products_copy = mask_products;
     vector<product> products_copy = products;
-    unsigned int result_copy = result;
-    unsigned int partial_products_copy = partial_products;
+    int result_copy = result;
+    int partial_products_copy = partial_products;
     unsigned  int quantity_products_copy = quantity_products;
 
 
@@ -132,7 +130,7 @@ void backtracking(int i = 0){
     // optimalidad me huele raro
     //poda optimalidad O(1)
     if (optimalidad) {
-        unsigned int max_possible_res = partial_products + quantity_products - i;
+        int max_possible_res = partial_products + quantity_products - i;
         if (max_possible_res <= result) return;
     }
 
@@ -141,9 +139,10 @@ void backtracking(int i = 0){
 
         // O(2.n)
         if (not jambotube_broke(i)){                                         //O(n)
-            unsigned int local_result = 0;
+            int local_result = 0;
             local_result = quantity_products_jambotubo();                   //O(n)
-            result = (local_result > result)? local_result : result;
+            result = min(local_result, result);
+//            result = (local_result > result)? local_result : result;
         }
 
     } else {
@@ -159,14 +158,38 @@ void backtracking(int i = 0){
 }
 
 
+
+
+
+vector<vector<int>> M;
+int pd(int i = 0, int r = 0, int w = 0, int acum = 0){
+
+    vector<product> products_copy = products;
+
+    if (i == quantity_products){
+        if(w > resistance or r < 0) return 0;                 //!fijarse si r < 0 es estricto o no
+        return acum;
+    }
+
+    //caso agrego
+    int ra = products[i].resistance;
+    int wa = w + products[i].weight;
+    if (acum != 0) ra = min(ra, r - wa); // me quedo con min r ó la r que ya venia - peso agregado o  resist. del producto actual, w != 0 salva al caso base donde no haya ningun elemento aun, sino queda en loop de 0
+    return max( pd(i+1,r , w, acum), pd(i+1, ra, wa, acum + 1) );
+}
+
+
+
+
+
 int main() {
 
     // pedir datos
     cin >> quantity_products;
     cin >> resistance;
     for (int i = 0; i < quantity_products; ++i) {
-        unsigned int weight;
-        unsigned int resistance;
+        int weight;
+        int resistance;
         cin >> weight;
         cin >> resistance;
         product actual_product;
@@ -177,9 +200,8 @@ int main() {
     }
 
 //    brute_force();
-//    cout << "result: "<< result << endl;
-
-    backtracking();
+//    backtracking();
+    pd();
     cout << "result: "<< result << endl;
 
     cout << "entro: "<< aux_count << " veces"<< endl;
